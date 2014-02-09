d64b5391f13022c53080916308aed53455d27297
Nelson Elhage <nelhage@nelhage.com>
2014-02-09 15:14:17 -0800

    Add the git daemon.

--- cookbooks/nelhage/recipes/gitolite.rb
+++ cookbooks/nelhage/recipes/gitolite.rb
@@ -2,3 +2,12 @@ package "gitolite" do
   action :install
   response_file "gitolite.seed.erb"
 end
+
+package "openbsd-inetd" do
+  action :upgrade
+end
+
+execute "run git daemon" do
+  command "update-inetd --group OTHER --add 'git\tstream\ttcp\tnowait\tgit\t/usr/bin/git git daemon --inetd --base-path=/opt/git/repositories/ --syslog --verbose'"
+  not_if "grep -q 'git daemon' /etc/inetd.conf"
+end
