9e5dd80a512c1cfdb1dfe3ffaea9cfbdb9a4598e
Nelson Elhage <nelhage@nelhage.com>
2014-02-10 22:34:14 -0800

    Fix ordering issue with groups.

--- cookbooks/nelhage/recipes/default.rb
+++ cookbooks/nelhage/recipes/default.rb
@@ -8,9 +8,10 @@
 #
 
 include_recipe "nelhage::nginx"
-include_recipe "nelhage::users"
 include_recipe "nelhage::packages"
 include_recipe "nelhage::gitolite"
 
 include_recipe "golang"
 golang_package 'github.com/nelhage/daemonlogger'
+
+include_recipe "nelhage::users"
