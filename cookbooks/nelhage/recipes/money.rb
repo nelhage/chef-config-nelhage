20ccb203417e23edadafe9123af4aecde51a2574
Nelson Elhage <nelhage@nelhage.com>
2014-02-11 08:50:00 -0800

    Factor out daemontools job config.

--- cookbooks/nelhage/recipes/money.rb
+++ cookbooks/nelhage/recipes/money.rb
@@ -2,7 +2,10 @@ include_recipe "nodejs"
 
 daemontools_service "money-srv" do
   directory "/etc/sv/money-srv"
-  template 'money-srv'
+  template 'generic'
+  variables :service => 'money-srv',
+            :user => 'nelhage',
+            :command => 'node /opt/services/nelhage.com-money/serve.js'
   action [ :enable, :start ]
   log true
 end
