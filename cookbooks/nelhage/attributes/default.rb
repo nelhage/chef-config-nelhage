c94a61efbd1c843b15ef517c317f604d404b1945
Nelson Elhage <nelhage@nelhage.com>
2014-02-11 09:03:10 -0800

    Some updates

--- cookbooks/nelhage/attributes/default.rb
+++ cookbooks/nelhage/attributes/default.rb
@@ -1,2 +1,3 @@
 default['nginx']['default_site_enabled'] = false
 default['daemontools']['install_method'] = 'package'
+default['nodejs']['install_method'] = 'package'
--- cookbooks/nelhage/recipes/packages.rb
+++ cookbooks/nelhage/recipes/packages.rb
@@ -7,6 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 
+apt_repository 'cassou_emacs' do
+  uri 'http://ppa.launchpad.net/cassou/emacs/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver "keyserver.ubuntu.com"
+  key "8FBB7709281B6E626DD855CF84DBCE2DCEC45805"
+  action :add
+  notifies :run, "execute[apt-get update]", :immediately
+end
+
 %w[emacs24-nox].each do |pkg|
   package pkg do
     action :upgrade
