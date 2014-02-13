382fd78ace842f4b1f08e22eb79c69440c3c889a
Nelson Elhage <nelhage@nelhage.com>
2014-02-12 21:23:39 -0800

    mosh

--- cookbooks/nelhage/recipes/packages.rb
+++ cookbooks/nelhage/recipes/packages.rb
@@ -17,12 +17,22 @@ apt_repository 'cassou_emacs' do
   notifies :run, "execute[apt-get update]", :immediately
 end
 
+apt_repository 'mosh' do
+  uri 'http://ppa.launchpad.net/keithw/mosh/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver "keyserver.ubuntu.com"
+  key "F2923D3F37FEF230BDDC376E3EB9326A7BF6DFCD"
+  action :add
+  notifies :run, "execute[apt-get update]", :immediately
+end
+
 
 ZEPHYR_DEPS = %w[libzephyr4-krb5 libzephyr-dev comerr-dev krb5-user zephyr-clients]
 BUILD_DEPS = %w[build-essential texinfo pkg-config libperl-dev
    libssl-dev autoconf libncursesw5-dev libglib2.0-dev zip
    python-virtualenv liblocal-lib-perl python-dev]
-MISC_PACKAGES = %w[emacs24-nox ncdu stow tree mutt irssi]
+MISC_PACKAGES = %w[emacs24-nox ncdu stow tree mutt irssi mosh]
 
 (ZEPHYR_DEPS + BUILD_DEPS + MISC_PACKAGES).each do |pkg|
   package pkg do
