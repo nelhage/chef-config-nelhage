eb28f1ed07357bd69e4f1b04acddc8648bc97f2c
Nelson Elhage <nelhage@nelhage.com>
2014-02-20 09:07:14 -0800

    Send tarsnap output to logs, not mail.

--- cookbooks/nelhage/recipes/tarsnap.rb
+++ cookbooks/nelhage/recipes/tarsnap.rb
@@ -24,12 +24,12 @@ directory '/data/tarsnap.cache' do
   mode  '0700'
 end
 
-file '/usr/local/bin/nightly-tarsnap' do
-  action :create
+template 'nightly-tarsnap' do
+  path '/usr/local/bin/nightly-tarsnap'
+  source 'nightly-tarsnap.erb'
   owner 'root'
   group 'root'
   mode '0755'
-  content %q{/usr/local/bin/tarsnap --keyfile /etc/tarsnap/tarsnap.key --cachedir /data/tarsnap.cache/ -c -f "nelhage.com-$(date +%Y%m%d-%H%M)" /data/archived /data/backup /opt /data/home}
 end
 
 cron_d "tarsnap" do
