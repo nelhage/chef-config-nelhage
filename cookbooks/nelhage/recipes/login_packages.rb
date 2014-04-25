270912717971988bf70b38d4268534e7d0032f92
Nelson Elhage <nelhage@nelhage.com>
2014-04-25 09:40:09 -0600

    devbox: install mosh

--- cookbooks/nelhage/recipes/login_packages.rb
+++ cookbooks/nelhage/recipes/login_packages.rb
@@ -10,7 +10,7 @@ end
 
 %w[emacs24-nox build-essential scons python-software-properties
    python-virtualenv python-pip python-dev emacs24-nox
-   tree gdb].each do |p|
+   tree gdb mosh].each do |p|
   package p do
     action :upgrade
   end
