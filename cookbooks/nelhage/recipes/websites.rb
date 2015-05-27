b63ee3488cdcde54ff74838b74c146b247a24d49
Nelson Elhage <nelhage@nelhage.com>
2015-05-27 15:33:24 -0700

    Better cipher list for Logjam resilience.

--- cookbooks/nelhage/recipes/websites.rb
+++ cookbooks/nelhage/recipes/websites.rb
@@ -9,6 +9,15 @@ template 'ssl.conf' do
   notifies :reload, 'service[nginx]'
 end
 
+cookbook_file 'dhparam.pem' do
+  path '/etc/ssl/certs/dhparam.pem'
+  source 'dhparam.pem'
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :reload, 'service[nginx]'
+end
+
 def nginx_site(domain)
   template domain do
     path   "#{node['nginx']['dir']}/conf.d/#{domain}.conf"
