172a4fc59116872e95ec744fdb430f0df895a37f
Nelson Elhage <nelhage@nelhage.com>
2015-12-05 11:34:30 -0800

    Rip out the emacs PPA.

--- cookbooks/nelhage/recipes/login_packages.rb
+++ cookbooks/nelhage/recipes/login_packages.rb
@@ -1,15 +1,5 @@
-apt_repository 'cassou_emacs' do
-  uri 'http://ppa.launchpad.net/ubuntu-elisp/ppa/ubuntu'
-  distribution node['lsb']['codename']
-  components ['main']
-  keyserver "keyserver.ubuntu.com"
-  key "8FBB7709281B6E626DD855CF84DBCE2DCEC45805"
-  action :add
-  notifies :run, "execute[apt-get update]", :immediately
-end
-
 %w[emacs24-nox build-essential scons python-software-properties
-   python-virtualenv python-pip python-dev emacs24-nox
+   python-virtualenv python-pip python-dev
    tree gdb mosh].each do |p|
   package p do
     action :upgrade
