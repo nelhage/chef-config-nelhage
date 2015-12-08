084f60d3f5d1a4292b613064e608689f1c5aefd9
Nelson Elhage <nelhage@nelhage.com>
2015-12-07 16:57:13 -0800

    nuke partchat/zephyr bridge

--- cookbooks/nelhage/recipes/nelhage_com.rb
+++ cookbooks/nelhage/recipes/nelhage_com.rb
@@ -3,7 +3,6 @@ include_recipe "nelhage"
 include_recipe "nelhage::websites"
 include_recipe "nelhage::gitolite"
 include_recipe "nelhage::money"
-include_recipe "nelhage::party-zephyr"
 include_recipe "nelhage::tarsnap"
 
 include_recipe "nelhage::packages"
