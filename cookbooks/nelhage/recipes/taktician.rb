1404928b48c29c31d6ac573762811076665ec61f
Nelson Elhage <nelhage@nelhage.com>
2016-05-26 12:04:16 -0400

    set restart_sec on the logger

--- cookbooks/nelhage/recipes/taktician.rb
+++ cookbooks/nelhage/recipes/taktician.rb
@@ -1,6 +1,7 @@
 nelhage_service 'taklogger' do
   command '/home/nelhage/code/go/bin/taklogger -out /home/nelhage/code/go/src/github.com/nelhage/taktician/games'
   user 'nelhage'
+  restart_sec 5
 end
 
 node['tak'].each do |name, opts|
