192a634b64ca3138763a49a6d8ea192631768b77
Nelson Elhage <nelhage@nelhage.com>
2016-07-11 21:13:36 -0700

    taklogger: index games as it runs

--- cookbooks/nelhage/recipes/taktician.rb
+++ cookbooks/nelhage/recipes/taktician.rb
@@ -1,5 +1,5 @@
 nelhage_service 'taklogger' do
-  command '/home/nelhage/code/go/bin/taklogger -out /home/nelhage/code/go/src/github.com/nelhage/taktician/games'
+  command '/home/nelhage/code/go/bin/taklogger -out /home/nelhage/code/go/src/github.com/nelhage/taktician/games -index /home/nelhage/code/go/src/github.com/nelhage/taktician/games/games.db'
   user 'nelhage'
   restart_sec 5
 end
