b679f9a143aa5c66c17a8539e706bfb355747408
Nelson Elhage <nelhage@nelhage.com>
2016-07-28 12:23:52 -0700

    fix $myorigin

--- cookbooks/nelhage/attributes/default.rb
+++ cookbooks/nelhage/attributes/default.rb
@@ -1,6 +1,7 @@
 default['nginx']['default_site_enabled'] = false
 default['nodejs']['install_method'] = 'package'
 
+default['postfix']['main']['myorigin'] = '$mydomain'
 default['postfix']['main']['smtp_tls_CAfile'] = '/etc/ssl/certs/ca-certificates.crt'
 
 default['nelhage']['users']['extra_groups'] = %w[sudo]
